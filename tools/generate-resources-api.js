d9245f8b759201201fd00540da8cc148ab221c5d
jsonberry <jason.s.awbrey@gmail.com>
2019-08-03 12:07:03 -0700

    add target blank to anchors

--- tools/generate-resources-api.js
+++ tools/generate-resources-api.js
@@ -1,9 +1,15 @@
 const marked = require('marked');
 const matter = require('gray-matter');
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 const resourcesDir = path.resolve(__dirname, '../resources');
-const dist = path.resolve(__dirname, '../')
+const dist = path.resolve(__dirname, '../');
+
+const renderer = new marked.Renderer();
+
+renderer.link = function(href, title, link) {
+  return `<a href="${href}" target="_blank">${link}</a>`;
+};
 
 fs.readdir(resourcesDir, (err, files) => {
   const api = {};
@@ -22,15 +28,13 @@ fs.readdir(resourcesDir, (err, files) => {
       [id]: {
         id,
         ...frontMatter.data,
-        content: marked(frontMatter.content),
-      }
-    }
+        content: marked(frontMatter.content, { renderer }),
+      },
+    };
   });
 
   api.tags = [
-    ...new Set(
-      ...Object.keys(api.resources).map(id => api.resources[id].tags),
-    ),
+    ...new Set(...Object.keys(api.resources).map(id => api.resources[id].tags)),
   ];
 
   fs.writeFileSync(`${dist}/api.json`, JSON.stringify(api), 'utf8');
